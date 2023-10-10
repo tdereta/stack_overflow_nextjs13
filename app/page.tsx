4b65d6dd3e196295eb8d3c36ccd41ecb4ab3be7d
Teo Dereta <tdereta@gmail.com>
2023-10-10 12:14:47 +0200

    Authentication with Clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Here we go with Next.js 13!</h1>
-      <h2 className="h2-bold">Here we go with Next.js 13!</h2>
-      <h3 className="h3-bold">Here we go with Next.js 13!</h3>
-    </div>
-  )
-}
