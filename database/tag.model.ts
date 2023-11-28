936ff1036d95413431e48678367081be98873445
Teo Dereta <tdereta@gmail.com>
2023-11-28 17:37:19 +0100

    edit tag model

--- database/tag.model.ts
+++ database/tag.model.ts
@@ -1,41 +1,21 @@
-import { Schema, models, model, Document } from "mongoose"
+import { Schema, model, models, Document } from 'mongoose';
 
 export interface ITag extends Document {
-    name: string
-    description: string
-    questions: Schema.Types.ObjectId[]
-    followers: Schema.Types.ObjectId[]
-    createdAt: Date
+    name: string;
+    description: string;
+    questions: Schema.Types.ObjectId[];
+    followers: Schema.Types.ObjectId[];
+    createdOn: Date;
 }
 
-const TagSchema = new Schema<ITag>({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    questions: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Question"
-        }
-    ],
-    followers: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+const TagSchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    createdOn: { type: Date, default: Date.now },
+});
 
-const Tag = models.User || model("Tag", TagSchema)
+const Tag = models.Tag || model('Tag', TagSchema);
 
-export default Tag
+export default Tag;
